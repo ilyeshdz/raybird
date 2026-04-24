4ff8784fe8038e63cfa4faa221d8cadc8bf73e2c
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-04-24 21:30:10 +0200

    feat: create a window and basic lifecycle

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include <raylib.h>
 
 int main() {
-  printf("Hello from C");
+  const int width = 1280;
+  const int height = 720;
+
+  InitWindow(width, height, "hello");
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(GRAY);
+    DrawText("hello", 190, 200, 20, WHITE);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return 0;
 }
