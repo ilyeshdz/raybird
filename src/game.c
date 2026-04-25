dc2368bd31351667a969e715865ee338232b31e4
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-04-25 13:38:28 +0200

    feat: setup gravity to the player

--- src/game.c
+++ src/game.c
@@ -1,6 +1,7 @@
 #include "game.h"
 #include "player.h"
 #include "raylib.h"
+#include <stdio.h>
 
 GameState InitGame() {
   return (GameState){.win = false,
@@ -10,9 +11,13 @@ GameState InitGame() {
                                 .jumpForce = 400.0,
                                 .velocity = {.x = 0, .y = 0}}};
 }
-GameState InitGame() { return (GameState){.win = false}; };
-
-void UpdateGame(GameState *state) {}
+void UpdateGame(GameState *state) {
+  if (IsKeyPressed(KEY_SPACE)) {
+    state->player.velocity.y = -state->player.jumpForce;
+  }
+  state->player.velocity.y += state->player.gravity * GetFrameTime();
+  state->player.position.y += state->player.velocity.y * GetFrameTime();
+}
 void DrawGame(GameState *state) {
   BeginDrawing();
   Rectangle player = GetPlayerRect(&state->player);
