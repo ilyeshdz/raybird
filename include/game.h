17c4ec9eafc15c9c2b1fb2795f476362fb708ca0
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-04-25 16:14:15 +0200

    feat: create and render 5 pipes

--- include/game.h
+++ include/game.h
@@ -1,11 +1,25 @@
 #ifndef GAME_H
 #define GAME_H
 #include "player.h"
+#include "raylib.h"
 #include <stdbool.h>
+
+#define PIPE_GAP 300
+#define MAX_PIPES 5
+#define PIPE_WIDTH 100
+#define PIPE_HEIGHT 600
+#define PIPE_SPACE 400
+
+typedef struct {
+  Rectangle topRect;
+  Rectangle bottomRect;
+  float xPos;
+} Pipe;
 
 typedef struct {
   bool win;
   Player player;
+  Pipe pipes[MAX_PIPES];
 } GameState;
 
 GameState InitGame();
--- src/game.c
+++ src/game.c
@@ -5,7 +5,7 @@
 
 GameState InitGame() {
   const float player_size = 100.0f;
-  return (GameState){
+  GameState game_state = {
       .win = false,
       .player = {.position = {.y = (GetScreenHeight() - player_size) / 2.0f,
                               .x = (GetScreenWidth() - player_size) / 2.0f},
@@ -13,17 +13,44 @@ GameState InitGame() {
                  .gravity = 500.0,
                  .jumpForce = 400.0,
                  .velocity = {.x = 0, .y = 0}}};
+
+  for (int i = 0; i < MAX_PIPES; i++) {
+    game_state.pipes[i].xPos = GetScreenWidth() + (i * PIPE_SPACE);
+    int minGapY = 100;
+    int maxGapY = GetScreenHeight() - PIPE_WIDTH - PIPE_GAP;
+    int gapCenterY = GetRandomValue(minGapY, maxGapY);
+
+    game_state.pipes[i].topRect = (Rectangle){.x = game_state.pipes[i].xPos,
+                                              .y = gapCenterY - PIPE_HEIGHT,
+                                              .width = PIPE_WIDTH,
+                                              .height = 600};
+
+    game_state.pipes[i].bottomRect = (Rectangle){.x = game_state.pipes[i].xPos,
+                                                 .y = gapCenterY + PIPE_GAP,
+                                                 .width = PIPE_WIDTH,
+                                                 .height = PIPE_HEIGHT};
+  }
+
+  return game_state;
 }
 void UpdateGame(GameState *state) {
   if (IsKeyPressed(KEY_SPACE)) {
     PlayerJump(&state->player);
   }
   UpdatePlayer(&state->player);
+  for (int i = 0; i < MAX_PIPES; i++) {
+    state->pipes[i].bottomRect.x -= 200 * GetFrameTime();
+    state->pipes[i].topRect.x -= 200 * GetFrameTime();
+  }
 }
 void DrawGame(GameState *state) {
   BeginDrawing();
   Rectangle player = GetPlayerRect(&state->player);
-  DrawRectangleRec(player, LIGHTGRAY);
   ClearBackground(BLACK);
+  for (int i = 0; i < MAX_PIPES; i++) {
+    DrawRectangleRec(state->pipes[i].bottomRect, BLUE);
+    DrawRectangleRec(state->pipes[i].topRect, BLUE);
+  }
+  DrawRectangleRec(player, LIGHTGRAY);
   EndDrawing();
 }
