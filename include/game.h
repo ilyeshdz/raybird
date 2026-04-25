4023d76f9394255213283ca898e7862b591025f5
Ilyes Hernandez <hdz.ilyes@gmail.com>
2026-04-25 22:38:15 +0200

    refactor(game): remove pipe definitions from game.h
    Move pipe-related constants and Pipe struct to pipes.h.
    Add include for pipes.h.
    Remove InitPipe function declaration (now in pipes.h).
    Keep only game-specific definitions (PLAYER_SIZE, SCORE_INCREMENT).

--- include/game.h
+++ include/game.h
@@ -1,25 +1,13 @@
 #ifndef GAME_H
 #define GAME_H
 #include "player.h"
+#include "pipes.h"
 #include "raylib.h"
 
-#define PIPE_GAP 300
-#define MAX_PIPES 5
-#define PIPE_WIDTH 100
-#define PIPE_HEIGHT 600
-#define PIPE_SPACE 400
 #define PLAYER_SIZE 100.0f
-#define PIPE_SPEED 200.0f
 #define SCORE_INCREMENT 4
-#define MIN_GAP_Y 100
 
 typedef enum { GAME_RUNNING, GAME_OVER } GameStatus;
-
-typedef struct {
-  Rectangle topRect;
-  Rectangle bottomRect;
-  float xPos;
-} Pipe;
 
 typedef struct {
   GameStatus status;
@@ -32,6 +20,5 @@ GameState InitGame();
 void UpdateGame(GameState *state);
 void RestartGame(GameState *state);
 void DrawGame(GameState *state);
-void InitPipe(Pipe *pipe, float xPos);
 
 #endif
